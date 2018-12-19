acadc18001344e3b4369abd12583bff56e1758f6
shiv <shiv@revinfotech.com>
2018-12-19 17:26:12 +0530

    Basics of Python

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+blockchain = []
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
+
+add_value(get_user_input())
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=get_user_input())
+
+print(blockchain)
